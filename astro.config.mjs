a9fd115d278c14c870fece568a8c7fb73be8afcb
Ericsen Tsai <bommer95175@gmail.com>
2025-06-28 21:02:23 +0800

    fix: remove redundant import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
-import zeabur from "@zeabur/astro-adapter/serverless";
-
 import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
