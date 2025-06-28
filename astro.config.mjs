a670f314b7f86b3667054d2c66ce64e7d9938135
Ericsen Tsai <bommer95175@gmail.com>
2025-06-28 20:55:50 +0800

    build: update node adapter to zeabur

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
-import node from "@astrojs/node";
+import zeabur from "@zeabur/astro-adapter/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: zeabur(),
   output: "server",
 });
